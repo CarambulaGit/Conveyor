0de6512c12bccd5ecd10d49e862beee4f13c3e1a
Carambula <antibog6@gmail.com>
2021-06-02 12:40:01 +0300

    add conveyor script

---
+++ Assets/Project/Scripts/Conveyor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts {
+    public class Conveyor : MonoBehaviour {
+        [SerializeField] private new Rigidbody rigidbody;
+        [SerializeField] private float speed;
+
+        void FixedUpdate() {
+            Move();
+        }
+
+        void Move() {
+            var oldPos = rigidbody.position;
+            var pos = -transform.forward * (speed * Time.fixedDeltaTime);
+            rigidbody.position += pos;
+            rigidbody.MovePosition(oldPos);
+        }
+    }
+}
