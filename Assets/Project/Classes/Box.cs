3317444a4ff927c112835ae592755ed1ac53f734
Carambula <antibog6@gmail.com>
2021-06-04 00:49:33 +0300

    add box class and controller script, update prefab

---
+++ Assets/Project/Classes/Box.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Project.Classes {
+    public class Box {
+        public Transform Transform { get; private set; }
+
+        public Box(Transform transform) {
+            Transform = transform;
+        }
+    }
+}
---
+++ Assets/Project/Scripts/BoxController.cs
@@ -0,0 +1,18 @@
+﻿using Project.Classes;
+using UnityEngine;
+
+namespace Project.Scripts {
+    public class BoxController : MonoBehaviour {
+        [SerializeField] private Transform boxPrefab;
+        [SerializeField] private Transform conveyorTransform;
+
+        private Box _box;
+        private Vector3 _vectorUp = 3 * Vector3.up; 
+
+        public Box CreateBox() {
+            var box = Instantiate(boxPrefab, conveyorTransform.position + _vectorUp, boxPrefab.rotation); // todo
+            _box = new Box(box);
+            return _box;
+        }
+    }
+}
