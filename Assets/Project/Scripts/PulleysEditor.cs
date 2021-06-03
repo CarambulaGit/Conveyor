e0c6fd63ec9d03169a26a1498e449135938584e9
Carambula <antibog6@gmail.com>
2021-06-04 00:50:36 +0300

    add editor scripts for pulleys and belt sizes

---
+++ Assets/Project/Scripts/PulleysEditor.cs
@@ -0,0 +1,26 @@
+﻿using Project.Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace Project.EditorScripts {
+    // [ExecuteAlways]
+    [ExecuteInEditMode]
+    public class PulleysEditor : MonoBehaviour {
+        public Transform[] pulleys = new Transform[2];
+        public Transform belt;
+
+        private Vector3 _prevBeltScale;
+
+#if UNITY_EDITOR
+        public void Update() {
+            if (belt.localScale.Equals(_prevBeltScale)) return;
+            _prevBeltScale = belt.localScale;
+            var posZ = _prevBeltScale.z / 2;
+            var localPos0 = pulleys[0].localPosition;
+            var localPos1 = pulleys[1].localPosition;
+            pulleys[0].localPosition = new Vector3(localPos0.x, localPos0.y, posZ);
+            pulleys[1].localPosition = new Vector3(localPos1.x, localPos1.y, -posZ);
+        }
+#endif
+    }
+}
---
+++ Assets/Project/Scripts/SideBeltEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts {
+    [ExecuteInEditMode]
+    public class SideBeltEditor : MonoBehaviour {
+        public Transform[] sideBelt = new Transform[2];
+        public Transform belt;
+
+        private Vector3 _prevBeltScale;
+#if UNITY_EDITOR
+        public void Update() {
+            if (belt.localScale.Equals(_prevBeltScale)) return;
+            sideBelt[0].localScale =
+                new Vector3(sideBelt[0].localScale.x, sideBelt[0].localScale.y, 1 / belt.localScale.z);
+            sideBelt[1].localScale =
+                new Vector3(sideBelt[1].localScale.x, sideBelt[1].localScale.y, 1 / belt.localScale.z);
+        }
+#endif
+    }
+}
