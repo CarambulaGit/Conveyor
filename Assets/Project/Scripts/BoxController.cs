e8dfd181aef7c751a4ec237b93aad315482875c6
Carambula <antibog6@gmail.com>
2021-06-04 17:41:41 +0300

    update box and target controllers

--- Assets/Project/Scripts/BoxController.cs
+++ Assets/Project/Scripts/BoxController.cs
@@ -1,18 +1,32 @@
-﻿using Project.Classes;
+﻿using System;
+using Project.Classes;
 using UnityEngine;
 
 namespace Project.Scripts {
     public class BoxController : MonoBehaviour {
+        [SerializeField] private Conveyor conveyor;
         [SerializeField] private Transform boxPrefab;
-        [SerializeField] private Transform conveyorTransform;
+        [SerializeField] private Transform belt;
 
-        private Box _box;
-        private Vector3 _vectorUp = 3 * Vector3.up; 
+        private readonly Vector3 _vectorUp = 3 * Vector3.up;
+
+        public Box _box { get; private set; }
+
+        private void FixedUpdate() {
+            _box?.CheckDistance();
+        }
 
         public Box CreateBox() {
-            var box = Instantiate(boxPrefab, conveyorTransform.position + _vectorUp, boxPrefab.rotation); // todo
-            _box = new Box(box);
+            var box = Instantiate(boxPrefab, conveyor.transform.position + _vectorUp, boxPrefab.rotation); // todo
+            _box = new Box(box, conveyor, belt, RemoveBox);
             return _box;
         }
+
+        private void RemoveBox() {
+            Destroy(_box.Transform.gameObject);
+            _box = null;
+        }
+
+        // todo event of falling 
     }
 }
--- Assets/Project/Scripts/TargetController.cs
+++ Assets/Project/Scripts/TargetController.cs
@@ -7,29 +7,26 @@ namespace Project.Scripts {
         [SerializeField] private Transform targetPrefab;
         [SerializeField] private Transform belt;
 
-        private Target _target;
-
-        private void Awake() {
-            // CreateTarget();
-        }
+        public Target _target { get; private set; }
 
         private void FixedUpdate() {
             if (_target != null) {
                 MoveTarget(_target);
             }
         }
+        
+        public Target CreateTarget() {
+            var target = Instantiate(targetPrefab, new Vector3(0, 1.3f, -4.79f), targetPrefab.rotation); // todo
+            _target = new Target(target, conveyor, belt, RemoveTarget);
+            return _target;
+        }
 
         private void MoveTarget(Target target) {
             target.Move(Time.fixedDeltaTime);
         }
 
-        public Target CreateTarget() {
-            var target = Instantiate(targetPrefab, new Vector3(0, 1.3f, -4.79f), targetPrefab.rotation); // todo
-            _target = new Target(target, conveyor, belt, ClearTargetValue);
-            return _target;
-        }
-
-        private void ClearTargetValue() {
+        private void RemoveTarget() {
+            Destroy(_target.Transform.gameObject);
             _target = null;
         }
     }
