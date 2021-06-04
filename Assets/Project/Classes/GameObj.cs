01786a2a30b926bd216bbe9167afae0f86199faa
Carambula <antibog6@gmail.com>
2021-06-04 17:39:51 +0300

    add GameObj class

---
+++ Assets/Project/Classes/GameObj.cs
@@ -0,0 +1,27 @@
+﻿using Project.Scripts;
+using UnityEngine;
+
+namespace Project.Classes {
+    public class GameObj {
+        public Transform Transform { get; protected set; }
+        public Conveyor Conveyor { get; protected set; }
+        public Transform Belt { get; protected set; }
+        
+        public delegate void NeedToDestroy();
+
+        protected event NeedToDestroy OnNeedToDestroyObj;
+
+        public GameObj(Transform transform, Conveyor conveyor, Transform belt, NeedToDestroy onNeedToDestroy) {
+            Transform = transform;
+            Conveyor = conveyor;
+            Belt = belt;
+            OnNeedToDestroyObj += onNeedToDestroy;
+        }
+        
+        public void CheckDistance() {
+            if (Vector3.Distance(Transform.position, Conveyor.transform.position) > Belt.localScale.z / 2) {
+                OnNeedToDestroyObj?.Invoke();
+            }
+        }
+    }
+}
