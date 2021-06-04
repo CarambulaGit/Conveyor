4c7cf29ad5383dacb2f3b462cb8b9dd20b50ce3b
Carambula <antibog6@gmail.com>
2021-06-04 17:41:07 +0300

    make Box and Target inheritors of GameObj

--- Assets/Project/Classes/Box.cs
+++ Assets/Project/Classes/Box.cs
@@ -1,11 +1,9 @@
-﻿using UnityEngine;
+﻿using Project.Scripts;
+using UnityEngine;
 
 namespace Project.Classes {
-    public class Box {
-        public Transform Transform { get; private set; }
-
-        public Box(Transform transform) {
-            Transform = transform;
-        }
+    public class Box : GameObj {
+        public Box(Transform transform, Conveyor conveyor, Transform belt, NeedToDestroy onNeedToDestroy) : base(
+            transform, conveyor, belt, onNeedToDestroy) { }
     }
 }
--- Assets/Project/Classes/Target.cs
+++ Assets/Project/Classes/Target.cs
@@ -2,33 +2,13 @@
 using UnityEngine;
 
 namespace Project.Classes {
-    public class Target {
-        public Transform Transform { get; private set; }
-        public Conveyor Conveyor { get; private set; }
-        public Transform Belt { get; private set; }
-        
-        public delegate void TargetDestroy();
-        
-        public event TargetDestroy OnDestroyTarget;
-
-        public Target(Transform transform, Conveyor conveyor, Transform belt, TargetDestroy onTargetDestroy) {
-            Transform = transform;
-            Conveyor = conveyor;
-            Belt = belt;
-            OnDestroyTarget += onTargetDestroy;
-        }
+    public class Target : GameObj {
+        public Target(Transform transform, Conveyor conveyor, Transform belt, NeedToDestroy onNeedToDestroy) : base(
+            transform, conveyor, belt, onNeedToDestroy) { }
 
         public void Move(float fixedDeltaTime) {
-            if (Vector3.Distance(Transform.position, Conveyor.transform.position) > Belt.localScale.z / 2) {
-                RemoveTarget();
-            }
+            CheckDistance();
             Transform.position += Conveyor.MoveDirection * (fixedDeltaTime * Conveyor.GetSpeed());
-        }
-
-        private void RemoveTarget() {
-            // Transform.gameObject.SetActive(false);
-            OnDestroyTarget?.Invoke();
-            GameManager.Instance.DestroyGO(Transform.gameObject);
         }
     }
 }
