22df84cbd393e7af759ad3e6760dc6a2fda36d96
Carambula <antibog6@gmail.com>
2021-06-06 17:30:21 +0300

    add physics interaction of box with conveyor

--- Assets/Project/Scripts/BoxController.cs
+++ Assets/Project/Scripts/BoxController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 namespace Project.Scripts {
     public class BoxController : MonoBehaviour {
-        private string conveyorTag = "Conveyor";
-        private string beltTag = "Belt";
-        private Conveyor conveyor;
-        private Transform belt;
-
-        public Box _box { get; private set; }
+        private readonly string _conveyorTag = "Conveyor";
+        private readonly string _beltTag = "Belt";
+        private Conveyor _conveyor;
+        private Transform _belt;
         private Target _connectedTarget;
+        private bool _alreadyCalculated;
+
+        public Box Box { get; private set; }
 
         public Target ConnectedTarget {
             get => _connectedTarget;
@@ -20,24 +21,42 @@ namespace Project.Scripts {
             }
         }
 
+        public bool StillNotFall { get; private set; } = true;
 
         private void Awake() {
-            conveyor = GameObject.FindWithTag(conveyorTag).GetComponent<Conveyor>();
-            belt = GameObject.FindWithTag(beltTag).transform;
+            _conveyor = GameObject.FindWithTag(_conveyorTag).GetComponent<Conveyor>();
+            _belt = GameObject.FindWithTag(_beltTag).transform;
         }
 
         private void FixedUpdate() {
-            _box?.CheckDistance();
+            Box?.CheckDistance();
         }
 
         public Box CreateBox() {
-            _box = new Box(transform, conveyor, belt, this, RemoveBox);
-            return _box;
+            var rb = transform.GetComponent<Rigidbody>();
+            Box = new Box(transform, _conveyor, _belt, this, rb, DestroyBox);
+            return Box;
         }
 
-        private void RemoveBox() {
-            Destroy(_box.Transform.gameObject);
-            _box = null;
+        public void DestroyBox() {
+            Destroy(Box.Transform.gameObject);
+            Box = null;
+        }
+
+        public void DisableKinematic() {
+            Box.Rigidbody.isKinematic = false;
+        }
+
+        private void OnCollisionEnter(Collision other) {
+            if (_alreadyCalculated) return;
+
+            if (other.gameObject.CompareTag(_conveyorTag)) {
+                StillNotFall = false;
+                GameManager.Instance.CalculateScore(this);
+                _alreadyCalculated = true;
+                ConnectedTarget.TargetController.RemoveTarget();
+                GameManager.Instance.CreatePair();
+            }
         }
 
         // todo event of falling 
