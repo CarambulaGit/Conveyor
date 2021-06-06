ac1ba02c4e8e45c2fd17ed72b0f15cb821a2f904
Carambula <antibog6@gmail.com>
2021-06-06 17:30:42 +0300

    add rigidbody field to Box

--- Assets/Project/Classes/Box.cs
+++ Assets/Project/Classes/Box.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 namespace Project.Classes {
     public class Box : GameObj {
         public BoxController BoxController { get; private set; }
+        public Rigidbody Rigidbody { get; private set; }
 
         public Box(Transform transform, Conveyor conveyor, Transform belt, BoxController boxController,
-            NeedToDestroy onNeedToDestroy) : base(transform, conveyor, belt, onNeedToDestroy) {
+            Rigidbody rigidbody, NeedToDestroy onNeedToDestroy) : base(transform, conveyor, belt, onNeedToDestroy) {
             BoxController = boxController;
+            Rigidbody = rigidbody;
         }
     }
 }
