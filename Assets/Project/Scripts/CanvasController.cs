97e346ad19255c3bd93690f8f8b05ef146434283
Carambula <antibog6@gmail.com>
2021-06-08 22:59:28 +0300

    add CanvasController

---
+++ Assets/Project/Scripts/CanvasController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts {
+    public class CanvasController : MonoBehaviour {
+        [SerializeField] private GameObject BeforeGame;
+        [SerializeField] private GameObject InGame;
+        [SerializeField] private GameObject AfterGame;
+        [SerializeField] private TextMeshProUGUI scoreValue;
+        [SerializeField] private TextMeshProUGUI timeValue;
+
+        public void UpdateScore() {
+            scoreValue.text = GameManager.Instance.Score.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public void UpdateTime() {
+            var time = TimeSpan.FromSeconds(GameManager.Instance.CurrentTime);
+            timeValue.text = $"{time.Minutes}:{time.Seconds}";
+        }
+
+        public void ActivateBeforeGame() {
+            ActivateMenu(true, false, false);
+        }
+
+        public void ActivateInGame() {
+            ActivateMenu(false, true, false);
+
+        }
+
+        public void ActivateAfterGame() {
+            ActivateMenu(false, false, true);
+        }
+
+        private void ActivateMenu(bool beforeGame, bool inGame, bool afterGame) {
+            BeforeGame.SetActive(beforeGame);
+            InGame.SetActive(inGame);
+            AfterGame.SetActive(afterGame);
+        }
+    }
+}
