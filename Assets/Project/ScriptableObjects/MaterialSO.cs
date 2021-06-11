d572456cd9bebbefe0734326e3d24e997aeb96ee
Carambula <antibog6@gmail.com>
2021-06-11 23:22:05 +0300

    add materialSO

---
+++ Assets/Project/ScriptableObjects/MaterialSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Project.ScriptableObjects {
+    [CreateAssetMenu(fileName = "New conveyor material", menuName = "ConveyorMaterial", order = 0)]
+    public class MaterialSO : ScriptableObject {
+        public int cost;
+        public Material material;
+    }
+}
