057e6d81391767c1abd726d01344750869de512e
Carambula <antibog6@gmail.com>
2021-06-11 23:20:27 +0300

    add Constants.cs

--- Assets/Project/Classes/Constants.cs
+++ Assets/Project/Classes/Constants.cs
@@ -1,5 +1,20 @@
 ﻿namespace Project.Classes {
     public static class Constants {
         public const float EPSILON = 0.001f;
+
+        #region SceneNames
+
+        public const string GAME_SCENE_NAME = "GameScene";
+        public const string MAIN_MENU_SCENE_NAME = "MainMenu";
+
+        #endregion
+
+        #region PlayerPrefsKeys
+
+        public const string COINS_KEY = "coins";
+        public const string MATERIALS_KEY = "materials";
+        public const string CURRENT_MATERIAL_KEY = "currentMat";
+
+        #endregion
     }
 }
