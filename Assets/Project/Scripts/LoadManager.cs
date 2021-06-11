de7fbd32ac774e5468c52553d31af9aa2e623249
Carambula <antibog6@gmail.com>
2021-06-11 23:27:57 +0300

    add LoadManager

---
+++ Assets/Project/Scripts/LoadManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Project.Classes;
+using UnityEngine;
+
+namespace Project.Scripts {
+    public class LoadManager : MonoBehaviour {
+        // private delegate void LoadData();
+        // private LoadData loadData;
+
+        private void Awake() {
+            // PlayerPrefs.DeleteAll(); // todo remove
+            Coins.Instance.Load();
+            // Coins.Instance.AddCoins(30);
+            ConveyorMaterial.Load();
+        }
+
+        // private void Update() {
+            // Debug.Log($"Coins = {Coins.Instance.CoinsValue}");
+            // Debug.Log($"Inventory = {(int) ConveyorMaterial.MaterialsInventory}");
+            // Debug.Log($"Cur = {(ConveyorMaterial.ConveyorMaterialsContent) ConveyorMaterial.CurrentMaterial}");
+        // }
+    }
+}
