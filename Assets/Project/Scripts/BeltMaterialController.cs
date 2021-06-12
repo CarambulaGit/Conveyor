d7bdba6b21f5f7d2105534f0dc603908d736ae07
Carambula <antibog6@gmail.com>
2021-06-13 02:59:37 +0300

    add script that change belt material

---
+++ Assets/Project/Scripts/BeltMaterialController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Project.Classes;
+using UnityEngine;
+
+namespace Project.Scripts {
+    public class BeltMaterialController : MonoBehaviour {
+        [SerializeField] private List<MeshRenderer> beltsParts;
+
+        private void Awake() {
+            var materialIndex = PlayerPrefs.GetInt(Constants.CURRENT_MATERIAL_KEY);
+            var conveyorMaterial = ConveyorMaterial.FindByIndex(materialIndex);
+            var curMaterial = conveyorMaterial.MaterialSO.material;
+            foreach (var meshRenderer in beltsParts) {
+                meshRenderer.material = curMaterial;
+            }
+        }
+    }
+}
